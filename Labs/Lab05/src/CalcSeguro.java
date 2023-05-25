5cb4899e2f80f44c712eefcb9d445f9dab598983
leticialopesms <l184423@dac.unicamp.br>
2023-05-25 13:12:40 -0300

    Criando pasta Lab05

---
+++ Labs/Lab05/src/CalcSeguro.java
@@ -0,0 +1,22 @@
+public enum CalcSeguro {
+    VALOR_BASE(100.0),
+    FATOR_18_30(1.2),
+    FATOR_30_60(1.0),
+    FATOR_60_90(1.5);
+
+
+    // Atributo
+    private final double valor;
+
+
+    // Construtor
+    CalcSeguro (double valor) {
+        this.valor = valor;
+    }
+
+    // Métodos
+    // - Getter
+    public double getValor() {
+        return valor;
+    }
+}
